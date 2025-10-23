d6d89ecae7c2e5f0205eeff069e6a39fe2cc49fc
uggenio <eielpa@outloook.com>
2025-10-23 15:30:09 +0200

    EC-12 Welcome page setup

--- energycommunities-ng/src/app/app.ts
+++ energycommunities-ng/src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: 'welcome/welcome.html',
   styleUrl: './app.css'
 })
 export class App {

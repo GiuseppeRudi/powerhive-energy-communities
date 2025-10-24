36cd5611016cc4d913c2932f6b91ad85badf7164
pierluigi <pierluigitrc@gmail.com>
2025-10-24 11:48:32 +0200

    EC-11-user-login logout implented

--- energycommunities/src/main/java/it/unical/demacs/asd/energycommunities/config/SecurityConfig.java
+++ energycommunities/src/main/java/it/unical/demacs/asd/energycommunities/config/SecurityConfig.java
@@ -19,17 +19,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable());
-		http.cors(cors -> cors.disable());
-        http.authorizeHttpRequests(authorize ->
+		http.csrf(csrf -> csrf.disable())
+		.cors(cors -> cors.disable())
+        .authorizeHttpRequests(authorize ->
             authorize.requestMatchers(
                 "/v3/api-docs/**", 
                 "/users/register",
                 "/swagger-ui/**"
             ).permitAll()
             .anyRequest().authenticated()
-        ).httpBasic(httpBasic -> httpBasic.init(http));
+        ).httpBasic(httpBasic -> httpBasic.init(http))
 
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessHandler((request, response, authentication) -> {
+                response.setStatus(200);
+                response.getWriter().write("Successifully logout");
+                response.getWriter().flush();
+            })  
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID"));
         
         return http.build();
     }

d7b97002483a32454cc944c4459d45a962870989
fracris <crsfnc03s26m208a@studenti.unical.it>
2025-10-29 11:29:00 +0100

    EC-15 completed table for mapping csv file into db

---
+++ energycommunities/src/main/java/it/unical/demacs/asd/energycommunities/data/entities/Member.java
@@ -0,0 +1,49 @@
+package it.unical.demacs.asd.energycommunities.data.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "members")
+public class Member {
+
+    public enum MemberType {
+        PRODUCER, CONSUMER, PROSUMER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private MemberType memberType;
+
+    public MemberType getMemberType() {
+        boolean hasProducer = profiles.stream().anyMatch(p -> p.getType() == Profile.ProfileType.PRODUCER);
+        boolean hasConsumer = profiles.stream().anyMatch(p -> p.getType() == Profile.ProfileType.CONSUMER);
+
+        if (hasProducer && hasConsumer) return MemberType.PROSUMER;
+        else if (hasProducer) return MemberType.PRODUCER;
+        else return MemberType.CONSUMER;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "plan_id", nullable = false)
+    @JsonIgnore
+    private Plan plan;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Profile> profiles;
+}

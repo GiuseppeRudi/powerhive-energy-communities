dc32d74868cceb2351786dd201b9e40bfca9eddb
uggenio <eielpa@outloook.com>
2025-11-01 22:04:21 +0100

    Added analysis angular component, updated login component

--- energycommunities-ng/src/app/app.routes.ts
+++ energycommunities-ng/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Welcome } from './components/welcome/welcome';
 import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { MemberOverview } from './components/member-overview/member-overview';
+import { AnalysisComponent } from './components/analysis/analysis.component';
 
 export const routes: Routes = [
   { path: '', component: Welcome },
   { path: 'login', component: Login },
   { path: 'dashboard', component: Dashboard },
   { path: 'member/:id', component: MemberOverview },
-  { path: 'csv', component: Csv}
+  { path: 'csv', component: Csv},
+  { path: 'analysis', component: AnalysisComponent}
 ];
---
+++ energycommunities-ng/src/app/components/analysis/analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis',
+  imports: [],
+  templateUrl: './analysis.component.html',
+  styleUrl: '../welcome/welcome.css'
+})
+export class AnalysisComponent {
+
+}

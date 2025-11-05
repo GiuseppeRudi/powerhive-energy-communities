dac0e440a3a0c3b63ef3be0a1c4a86b28ec21ff6
fracris <crsfnc03s26m208a@studenti.unical.it>
2025-11-05 17:35:30 +0100

    EC-33 added loader

---
+++ energycommunities-ng/src/app/components/generation-loader/generation-loader.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generation-loader',
+  imports: [],
+  templateUrl: './generation-loader.html',
+  styleUrl: './generation-loader.css',
+})
+export class GenerationLoader {
+
+}

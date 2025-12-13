d154e2ce8b48a9a73abd615b0691d8ec25343041
pierluigi <pierluigitrc@gmail.com>
2025-12-13 18:22:28 +0100

    EC-145 zoom implemented. Dynamic color implemented (only in member-overview)

--- energycommunities-ng/src/app/components/energy-chart/energy-chart.ts
+++ energycommunities-ng/src/app/components/energy-chart/energy-chart.ts
@@ -1,19 +1,58 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartData, ChartOptions, Chart, registerables, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import zoomPlugin from 'chartjs-plugin-zoom';
 
-Chart.register(...registerables);
+Chart.register(...registerables, zoomPlugin);
 
 @Component({
   selector: 'app-energy-chart',
   templateUrl: './energy-chart.html',
-  styleUrl: './energy-chart.css',
+  styleUrls: ['./energy-chart.css', '../welcome/welcome.css'],
   imports: [BaseChartDirective],
   standalone: true
 })
-export class EnergyChartComponent {
+export class EnergyChartComponent implements OnInit {
   @Input() data!: ChartData<any>;
   @Input() options!: ChartOptions<any>;
   @Input() selectedChartType: ChartType = 'line';
-}
 
+  @ViewChild(BaseChartDirective) private chart?: BaseChartDirective;
+
+  ngOnInit(): void {
+    this.options = {
+      ...this.options,
+      plugins: {
+        ...(this.options?.plugins || {}),
+        zoom: {
+          pan: {
+            enabled: true,
+            mode: 'xy',
+          },
+          zoom: {
+            wheel: {
+              enabled: false,
+              speed: 0.05
+            },
+            pinch: {
+              enabled: false
+            },
+            mode: 'xy',
+          }
+        }
+      }
+    };
+  }
+
+  public resetZoom(): void {
+    this.chart?.chart?.resetZoom();
+  }
+
+  public zoomIn(): void {
+    this.chart?.chart?.zoom(1.1);
+  }
+
+  public zoomOut(): void {
+    this.chart?.chart?.zoom(0.9);
+  }
+}
